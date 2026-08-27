8a639c089d1f69094c6dfb91a59809235f445554
yisuswtfx-afk <yisuswtf.x@gmail.com>
2026-08-27 17:00:38 -0500

    jesus4.py

---
+++ jesus4.py
@@ -0,0 +1,21 @@
+numero1 = float(input("ingrese su segundo numero: "))
+numero2 = float(input("ingrese su segundo numero: "))
+
+suma = numero1 + numero2
+
+resta = numero1 - numero2 
+
+multiplicacion = numero1 * numero2 
+
+if numero2 !=0:
+    division = numero1/numero2 
+    
+else:
+
+     division = ("no es dividible entre 0")
+     
+     
+print ("suma", suma)
+print ("resta", resta)
+print("multiplicacion", multiplicacion)
+print ("division", division)
