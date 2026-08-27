fc824e48c94152b48b29a26940b89d7e9c86cd65
yisuswtfx-afk <yisuswtf.x@gmail.com>
2026-08-27 17:05:54 -0500

    jesus8.py

---
+++ jesus8.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Ingrese su primera nota:"))
+nota2 = float(input("ingrese su segunda nota:"))
+nota3 = float(input("Ingrese su tercera nota:"))
+
+promedio = (nota1 + nota2 + nota3) / 3
+print("Nota 1:", nota1)
+print("Nota 2:", nota2)
+print("Nota 3:", nota3)
+print("Promedio:", promedio)
