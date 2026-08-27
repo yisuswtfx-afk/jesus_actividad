04e459d2004ee95d7ae0d2ea03066d65d17b3dc7
yisuswtfx-afk <yisuswtf.x@gmail.com>
2026-08-27 17:04:08 -0500

    Update jesus6.py

---
+++ jesus6.py
@@ -0,0 +1,5 @@
+nombre = input("ingrese su nombre: ")
+año_de_nacimiento = int(input("ingrese su año de nacimiento: "))
+año_actual = int(input("ingrese el año actual: "))
+edad = año_actual - año_de_nacimiento
+print( nombre, "tienes aproximadamente ", edad, "años ")
