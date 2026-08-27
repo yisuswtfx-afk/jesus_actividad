5379a6a3b28f09a9742864303aa780b42bd484f3
yisuswtfx-afk <yisuswtf.x@gmail.com>
2026-08-27 16:58:44 -0500

    jesus10.py

---
+++ jesus10.py
@@ -0,0 +1,8 @@
+dolares = float(input("Ingrese la cantidad de dólares: "))
+valor_dolar = float(input("Ingrese el valor del dólar en pesos colombianos: "))
+
+pesos = dolares * valor_dolar
+
+print("Dólares:", dolares)
+print("Valor del dólar:", valor_dolar)
+print("Pesos colombianos:", pesos)
